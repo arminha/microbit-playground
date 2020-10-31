3ee6d24956cd8def87bf169cb019a1295c8f45ed
Armin Häberling <armin.aha@gmail.com>
2020-10-31 14:42:49 +0100

    Write to UART

--- README.md
+++ README.md
@@ -13,3 +13,9 @@ Start an OpenOCD connection
 ```sh
 openocd -f interface/cmsis-dap.cfg -f target/nrf51.cfg -l /tmp/openocd.log
 ```
+
+Start minicom
+
+```sh
+minicom -D /dev/ttyACM0 minirc.dfl
+```
--- microrust-start/src/main.rs
+++ microrust-start/src/main.rs
@@ -4,15 +4,31 @@
 extern crate microbit;
 extern crate panic_semihosting;
 
+use core::fmt::Write;
 use cortex_m_rt::entry;
 use cortex_m_semihosting::hio;
-use core::fmt::Write;
+
+use microbit::hal::prelude::*;
+use microbit::hal::serial;
+use microbit::hal::serial::BAUD115200;
 
 #[entry]
 fn main() -> ! {
     let mut stdout = hio::hstdout().unwrap();
-    let language = "Rust";
-    let ranking = 1;
-    writeln!(stdout, "{} on embedded is #{}!", language, ranking).unwrap();
+    writeln!(stdout, "Start").unwrap();
+
+    if let Some(p) = microbit::Peripherals::take() {
+        let gpio = p.GPIO.split();
+        // Configure RX and TX pins accordingly
+        let tx = gpio.pin24.into_push_pull_output().into();
+        let rx = gpio.pin25.into_floating_input().into();
+        // Configure serial communication
+        let (mut tx, _) = serial::Serial::uart0(p.UART0, tx, rx, BAUD115200).split();
+
+        let language = "Rust";
+        let ranking = 1;
+        write!(tx, "{} on embedded is #{}!\r\n", language, ranking).unwrap();
+    }
+
     panic!("test-panic");
 }
